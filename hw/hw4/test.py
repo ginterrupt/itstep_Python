5c4d77e33942df6e7d578b3cf80cb5007a9b52f1
GiGeNCo <gigalfc@gmail.com>
2025-11-13 22:18:01 +0400

    Ignore hw directory

--- hw/hw4/test.py
+++
@@ -1,9 +0,0 @@
-class Person:
-    name = "Tom"
-
-    def display_info(self):
-        print("გამარჯობა! მე მქვია", self.name)
-
-
-person1 = Person()
-person1.display_info()  # გამარჯობა! მე მქვია Tom
